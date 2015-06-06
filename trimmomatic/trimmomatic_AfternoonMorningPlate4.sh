78ecb4ebcae4fa8c2dc4ac7cf88e4f5304f57343
Melis <akmanmelis@gmail.com>
2015-06-05 23:16:06 -0700

    trim

--- trimmomatic/trimmomatic_AfternoonMorningPlate4.sh
+++ trimmomatic/trimmomatic_AfternoonMorningPlate4.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/ProSNP/trimmomatic
 #SBATCH -J Trim4
-#SBATCH -o /home/makman/ProSNP/outs/trimmomatic_AfternoonMorning_stdout.txt
-#SBATCH -e /home/makman/ProSNP/outs/trimmomatic_AfternoonMorning_stderr.txt
+#SBATCH -o /home/makman/ProSNP/outs/trimmomatic_AfternoonMorningPlate4_stdout.txt
+#SBATCH -e /home/makman/ProSNP/outs/trimmomatic_AfternoonMorningPlate4_stderr.txt
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p bigmemh
+#SBATCH -p bigmemm
 #SBATCH --ntasks=32
-#SBATCH --array=0-1
+#SBATCH --array=0-2
 
 module load java
 java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCD/Afternoon_NoIndex_L002_R1_001.fastq.gz /home/makman/Rawdata/UCD/Afternoon_NoIndex_L002_R3_001.fastq.gz /home/makman/ProSNP/trimmomatic/Afternoon_forward.fastq.gz /home/makman/ProSNP/trimmomatic/Afternoon_forward_unmapped.fastq.gz /home/makman/ProSNP/trimmomatic/Afternoon_reverse.fastq.gz /home/makman/ProSNP/trimmomatic/Afternoon_reverse_unmapped.fastq.gz ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCD/Morning_NoIndex_L001_R1_001.fastq.gz /home/makman/Rawdata/UCD/Morning_NoIndex_L001_R3_001.fastq.gz /home/makman/ProSNP/trimmomatic/Morning_forward.fastq.gz /home/makman/ProSNP/trimmomatic/Morning_forward_unmapped.fastq.gz /home/makman/ProSNP/trimmomatic/Morning_reverse.fastq.gz /home/makman/ProSNP/trimmomatic/Morning_reverse_unmapped.fastq.gz ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allforward.fastq.gz  /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allreverse.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_forward.fastq.gz  /home/makman/ProSNP/trimmomatic/Plate4_forward_unmapped.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_reverse.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_reverse_unmapped.fastq.gz ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+
