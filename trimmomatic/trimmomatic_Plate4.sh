ba5f510086f3d245dafe97fcabe531d8835afea1
Melis <akmanmelis@gmail.com>
2015-06-05 22:33:18 -0700

    tr

---
+++ trimmomatic/trimmomatic_Plate4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -D /home/makman/ProSNP/trimmomatic
+#SBATCH -J Trim4
+#SBATCH -o /home/makman/ProSNP/outs/trimmomatic_Plate4_stdout.txt
+#SBATCH -e /home/makman/ProSNP/outs/trimmomatic_Plate4_stderr.txt
+#SBATCH --mail-type=All
+#SBATCH --mail-user=makman@ucdavis.edu
+#SBATCH -p bigmemh
+#SBATCH --ntasks=32
+#SBATCH --array=0-1
+
+module load java
+java -jar ./trimmomatic-0.33.jar PE ~/Rawdata/UCB/plate4_ALMA3  ~/Rawdata/UCB/plate4_ALMA3 /home/makman/ProSNP/trimmomatic/  /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
