9597ec647e5497107a3167867a8936c90536e0d3
Melis <akmanmelis@gmail.com>
2015-06-05 22:44:11 -0700

    trim

--- trimmomatic/trimmomatic_Plate4.sh
+++ trimmomatic/trimmomatic_Plate4.sh
@@ -5,9 +5,8 @@
 #SBATCH -e /home/makman/ProSNP/outs/trimmomatic_Plate4_stderr.txt
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p bigmemh
+#SBATCH -p med
 #SBATCH --ntasks=32
-#SBATCH --array=0-1
 
 module load java
-java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allforward.fastq.gz  /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allreverse.fastq.gz /home/makman/ProSNP/trimmomatic/  /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allforward.fastq.gz  /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allreverse.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_forward.fastq.gz  /home/makman/ProSNP/trimmomatic/Plate4_forward_unmapped.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_reverse.fastq.gz /home/makman/ProSNP/trimmomatic/Plate4_reverse_unmapped.fastq.gz ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
