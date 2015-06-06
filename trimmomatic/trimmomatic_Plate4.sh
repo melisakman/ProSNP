5ef8841bcff042cb8546e92a290370636f448063
Melis <akmanmelis@gmail.com>
2015-06-05 22:39:59 -0700

    trim

--- trimmomatic/trimmomatic_Plate4.sh
+++ trimmomatic/trimmomatic_Plate4.sh
@@ -10,4 +10,4 @@
 #SBATCH --array=0-1
 
 module load java
-java -jar ./trimmomatic-0.33.jar PE ~/Rawdata/UCB/plate4_ALMA3  ~/Rawdata/UCB/plate4_ALMA3 /home/makman/ProSNP/trimmomatic/  /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+java -jar ./trimmomatic-0.33.jar PE /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allforward.fastq.gz  /home/makman/Rawdata/UCB/plate4_ALMA3/Plate4_UCBdata_allreverse.fastq.gz /home/makman/ProSNP/trimmomatic/  /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ /home/makman/ProSNP/trimmomatic/ ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
