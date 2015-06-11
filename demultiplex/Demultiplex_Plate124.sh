d36835c443b546616199a2b0ca51072d73341c1a
Melis <akmanmelis@gmail.com>
2015-06-10 17:46:45 -0700

    dem

--- demultiplex/Demultiplex_Plate124.sh
+++ demultiplex/Demultiplex_Plate124.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/ProSNP/demultiplex/
 #SBATCH -J Demul124
-#SBATCH -o /home/makman/ProSNP/outs/demultiplex_57Plate4_stdout.txt
-#SBATCH -e /home/makman/ProSNP/outs/demultiplex_57Plate4_stderr.txt
+#SBATCH -o /home/makman/ProSNP/outs/demultiplex_Plate124_stdout.txt
+#SBATCH -e /home/makman/ProSNP/outs/demultiplex_Plate124_stderr.txt
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@ucdavis.edu
 #SBATCH -p med
 #SBATCH --ntasks=64
-#SBATCH --array=0-5
+#SBATCH --array=0-3
 module load FASTX-Toolkit
 module load perl
 cat /home/makman/ProSNP/trimmomatic/Plate1_2_forward.fastq | /home/makman/ProExp/fastx_barcode_splitter.pl --bcfile barcodes_Plate12.txt --bol --suffix .fastq.gz --mismatches 1
