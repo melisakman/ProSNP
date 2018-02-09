93d7dd4b5f5970c1f6ef4120e18e218d16a1e66a
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-08 23:34:19 -0800

    cat morning afternoon

--- old/demultiplex/Demultiplex_Plate124_2.sh
+++ old/demultiplex/Demultiplex_Plate124_2.sh
@@ -4,9 +4,9 @@
 #SBATCH -o /home/makman/ProSNP/outs/demultiplex_Plate124_2_stdout.txt
 #SBATCH -e /home/makman/ProSNP/outs/demultiplex_Plate124_2_stderr.txt
 #SBATCH --mail-type=All
-#SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p bigmemh
-#SBATCH --ntasks=32
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH -p serial
 module load FASTX-Toolkit
 module load perl
 cat /home/makman/ProSNP/trimmomatic/Plate1_2_reverse.fastq | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile barcodes_Plate12.txt --bol --prefix ProSNP2_ --suffix .fastq --mismatches 1
--- old/demultiplex/Demultiplex_Plate124_3.sh
+++ old/demultiplex/Demultiplex_Plate124_3.sh
@@ -4,9 +4,9 @@
 #SBATCH -o /home/makman/ProSNP/outs/demultiplex_Plate124_3_stdout.txt
 #SBATCH -e /home/makman/ProSNP/outs/demultiplex_Plate124_3_stderr.txt
 #SBATCH --mail-type=All
-#SBATCH --mail-user=makman@ucdavis.edu
-#SBATCH -p bigmemm
-#SBATCH --ntasks=32
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=makman@berkeley.edu
+#SBATCH -p serial
 module load FASTX-Toolkit
 module load perl
 cat /home/makman/ProSNP/trimmomatic/Plate4_forward.fastq | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile barcodes_Plate4.txt --bol --prefix ProSNP3_ --suffix .fastq --mismatches 1
