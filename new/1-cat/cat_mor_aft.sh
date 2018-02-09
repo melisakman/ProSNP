4e552817bef0ff7888c628b22263e53013ee5720
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-08 23:38:12 -0800

    cat morning afternoon

--- new/1-cat/cat_mor_aft.sh
+++ new/1-cat/cat_mor_aft.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=makman@berkeley.edu
-#SBATCH -p high
+#SBATCH -p bigmemm
 
 cat UCD/Afternoon_NoIndex_L002_R1_001.fastq.gz UCD/Morning_NoIndex_L001_R1_001.fastq.gz > UCD/plate12_R1.fastq.gz
 cat UCD/Afternoon_NoIndex_L002_R3_001.fastq.gz UCD/Morning_NoIndex_L001_R3_001.fastq.gz > UCD/plate12_R3.fastq.gz
