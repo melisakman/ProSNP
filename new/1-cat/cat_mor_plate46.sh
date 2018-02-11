bd84a510b9842a3732cf3b1c53c1b6b769334bcb
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-10 23:08:09 -0800

    demul

--- new/1-cat/cat_mor_plate46.sh
+++ new/1-cat/cat_mor_plate46.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/Rawdata/
-#SBATCH -J cat4-5-6
-#SBATCH -o /home/makman/ProSNP/outs/new/cat_plate456.out
-#SBATCH -e /home/makman/ProSNP/outs/new/cat_plate456.err
+#SBATCH -J cat4-6
+#SBATCH -o /home/makman/ProSNP/outs/new/cat_plate46.out
+#SBATCH -e /home/makman/ProSNP/outs/new/cat_plate46.err
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=makman@berkeley.edu
@@ -10,7 +10,5 @@
 
 cat NYU/ma_2_NoIndex_L002_R1_001.fastq.gz UCB/plate4_ALMA3/Plate4_UCBdata_allforward.fastq.gz > combined/plate4_R1.fastq.gz
 cat NYU/ma_2_NoIndex_L002_R2_001.fastq.gz UCB/plate4_ALMA3/Plate4_UCBdata_allreverse.fastq.gz > combined/plate4_R2.fastq.gz
-cat NYU/ma_3_NoIndex_L003_R1_001.fastq.gz NYU/ma_4_NoIndex_L004_R1_001.fastq.gz > combined/plate5_R1.fastq.gz
-cat NYU/ma_3_NoIndex_L003_R2_001.fastq.gz NYU/ma_4_NoIndex_L004_R2_001.fastq.gz > combined/plate5_R2.fastq.gz
 cat NYU/ma_5_NoIndex_L005_R1_001.fastq.gz NYU/2_NoIndex_L008_R1_001.fastq.gz > combined/plate6_R1.fastq.gz
 cat NYU/ma_5_NoIndex_L005_R2_001.fastq.gz NYU/2_NoIndex_L008_R2_001.fastq.gz > combined/plate6_R2.fastq.gz
--- new/1-cat/cat_plate12.sh
+++ new/1-cat/cat_plate12.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/Rawdata/
 #SBATCH -J Demul124
-#SBATCH -o /home/makman/ProSNP/outs/new/cat_mor_aft.out
-#SBATCH -e /home/makman/ProSNP/outs/new/cat_mor_aft.err
+#SBATCH -o /home/makman/ProSNP/outs/new/cat_plate12.out
+#SBATCH -e /home/makman/ProSNP/outs/new/cat_plate12.err
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH -p bigmemm
 
 cat UCD/Afternoon_NoIndex_L002_R1_001.fastq.gz UCD/Morning_NoIndex_L001_R1_001.fastq.gz > UCD/plate12_R1.fastq.gz
-cat UCD/Afternoon_NoIndex_L002_R3_001.fastq.gz UCD/Morning_NoIndex_L001_R3_001.fastq.gz > UCD/plate12_R3.fastq.gz
+cat UCD/Afternoon_NoIndex_L002_R3_001.fastq.gz UCD/Morning_NoIndex_L001_R3_001.fastq.gz > UCD/plate12_R2.fastq.gz
