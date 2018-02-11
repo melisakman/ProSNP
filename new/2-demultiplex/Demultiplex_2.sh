a22de70b8ed4b8b760058cc92ff36cc9ce020a32
Melis Akman <melisakman@Meliss-MacBook-Pro.local>
2018-02-10 23:35:01 -0800

    demul

--- new/2-demultiplex/Demultiplex_2.sh
+++ new/2-demultiplex/Demultiplex_2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/Rawdata/combined
-#SBATCH -J Demul1
-#SBATCH -o /home/makman/ProSNP/outs/demultiplex1.out
-#SBATCH -e /home/makman/ProSNP/outs/demultiplex1.err
+#SBATCH -J Demul2
+#SBATCH -o /home/makman/ProSNP/outs/demultiplex2.out
+#SBATCH -e /home/makman/ProSNP/outs/demultiplex2.err
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH -p bigmemm
 module load perl
-zcat plate12_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R1.fastq --mismatches 1
-zcat plate12_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R2.fastq --mismatches 1
-# zcat plate4_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R1.fastq --mismatches 1 | gzip
-# zcat plate4_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R2.fastq --mismatches 1 | gzip
+# zcat plate12_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R1.fastq --mismatches 1
+# zcat plate12_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R2.fastq --mismatches 1
+zcat plate4_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R1.fastq --mismatches 1 | gzip
+zcat plate4_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R2.fastq --mismatches 1 | gzip
 # zcat plate6_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate6.txt --bol --prefix ProSNP6_ --suffix _R1.fastq --mismatches 1 | gzip
 # zcat plate6_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate6.txt --bol --prefix ProSNP6_ --suffix _R2.fastq --mismatches 1 | gzip
 # zcat ../NYU/ma_3_NoIndex_L003_R1_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma3.txt --bol --prefix ProSNP5_ --suffix _R1.fastq --mismatches 1 | gzip
--- new/2-demultiplex/Demultiplex_4.sh
+++ new/2-demultiplex/Demultiplex_4.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 #SBATCH -D /home/makman/Rawdata/combined
-#SBATCH -J Demul1
-#SBATCH -o /home/makman/ProSNP/outs/demultiplex1.out
-#SBATCH -e /home/makman/ProSNP/outs/demultiplex1.err
+#SBATCH -J Demul4
+#SBATCH -o /home/makman/ProSNP/outs/demultiplex4.out
+#SBATCH -e /home/makman/ProSNP/outs/demultiplex4.err
 #SBATCH --mail-type=All
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=makman@berkeley.edu
 #SBATCH -p bigmemm
 module load perl
-zcat plate12_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R1.fastq --mismatches 1
-zcat plate12_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R2.fastq --mismatches 1
+# zcat plate12_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R1.fastq --mismatches 1
+# zcat plate12_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate12.txt --bol --prefix ProSNP12_ --suffix _R2.fastq --mismatches 1
 # zcat plate4_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R1.fastq --mismatches 1 | gzip
 # zcat plate4_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate4.txt --bol --prefix ProSNP4_ --suffix _R2.fastq --mismatches 1 | gzip
 # zcat plate6_R1.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate6.txt --bol --prefix ProSNP6_ --suffix _R1.fastq --mismatches 1 | gzip
 # zcat plate6_R2.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate6.txt --bol --prefix ProSNP6_ --suffix _R2.fastq --mismatches 1 | gzip
-# zcat ../NYU/ma_3_NoIndex_L003_R1_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma3.txt --bol --prefix ProSNP5_ --suffix _R1.fastq --mismatches 1 | gzip
-# zcat ../NYU/ma_3_NoIndex_L003_R2_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma3.txt --bol --prefix ProSNP5_ --suffix _R2.fastq --mismatches 1 | gzip
+zcat ../NYU/ma_3_NoIndex_L003_R1_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma3.txt --bol --prefix ProSNP5_ma3_ --suffix _R1.fastq --mismatches 1 | gzip
+zcat ../NYU/ma_3_NoIndex_L003_R2_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma3.txt --bol --prefix ProSNP5_ma3_ --suffix _R2.fastq --mismatches 1 | gzip
 # zcat ../NYU/ma_4_NoIndex_L004_R1_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma4.txt --bol --prefix ProSNP5_ --suffix _R1.fastq --mismatches 1 | gzip
 # zcat ../NYU/ma_4_NoIndex_L004_R2_001.fastq.gz | /home/makman/ProSNP/fastx_barcode_splitter.pl --bcfile /home/makman/ProSNP/demultiplex/barcodes_plate5_ma4.txt --bol --prefix ProSNP5_ --suffix _R2.fastq --mismatches 1 | gzip
