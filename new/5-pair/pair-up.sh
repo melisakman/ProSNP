dc64fdd2c543f63cbd34c59462e7d62b90343c86
melisakman <makman@berkeley.edu>
2018-07-12 00:25:46 -0700

    pair

--- new/5-pair/pair-up.sh
+++ new/5-pair/pair-up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH -D /home/makman/Rawdata/combined/
+#SBATCH -D /home/makman/Rawdata/combined
 #SBATCH -J star
 #SBATCH -o /home/makman/ProSNP/outs/pair-up.out
 #SBATCH -e /home/makman/ProSNP/outs/pair-up.err
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@berkeley.edu
-#SBATCH bigmemm
+#SBATCH -p bigmemm
 #SBATCH --mem=72GB
 #SBATCH --time=400:00:00
 
