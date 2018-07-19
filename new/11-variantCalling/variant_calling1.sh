4792abddb13154bbfb2fe70a91994db086ea6f9d
melisakman <makman@berkeley.edu>
2018-07-18 23:53:03 -0700

    variant calling

--- new/11-variantCalling/variant_calling1.sh
+++ new/11-variantCalling/variant_calling1.sh
@@ -19,6 +19,7 @@ java -Djava.io.tmpdir=/home/makman/temp/ -Xmx60G -jar /share/apps/GATK-3.6/Genom
 -T HaplotypeCaller \
 -R /home/makman/Protea_assembly/graph_prepens2_73_1kb.fa \
 -I ProSNP12_ALC_1045_markDups_RG.bam \
+--filter_reads_with_N_cigar \
 -stand_call_conf 20.0 -dontUseSoftClippedBases \
 -o ProSNP12_ALC_1045.vcf
 
