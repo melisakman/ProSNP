d2271e61d8565dcc96b8735db9b4a400f8637d16
melisakman <makman@berkeley.edu>
2018-08-01 15:04:41 -0700

    rm temp2

--- new/11-variantCalling/remove temp.sh
+++ new/11-variantCalling/remove temp.sh
@@ -5,8 +5,8 @@
 #SBATCH -e /home/makman/ProSNP/outs/remove_temp.err
 #SBATCH --mail-type=All
 #SBATCH --mail-user=makman@berkeley.edu
-#SBATCH -p bigmemm
-#SBATCH --mem=48GB
+#SBATCH -p bigmemh
+#SBATCH --mem=8GB
 #SBATCH --time=400:00:00
 
 rm -fr temp2
